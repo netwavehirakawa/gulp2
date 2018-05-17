96b5ffa0e07f6f766e41622d838e0748d7938be9
hirakawa_nw <hirakawa@netwave.co.jp>
2018-05-17 12:01:26 +0900

    新ファイルの追加
    gulpファイル

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var browserSync = require('browser-sync').create();
+
+//ブラウザ同期
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            index: "index.html"
+        }
+    });
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+//scssコンパイル
+gulp.task("sass", function() {
+    gulp.src("./css/style.scss")
+        .pipe(sass())
+        .pipe(autoprefixer({
+           browsers: ['last 2 versions'],
+           cascade: false
+       }))
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(gulp.dest("./css"))
+});
+
+//下記のファイルが変更されたらリロード
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch("./css/style.scss", ['sass']);
+    gulp.watch("./*.html", ['bs-reload']);
+    gulp.watch("./css/*.css", ['bs-reload']);
+});
